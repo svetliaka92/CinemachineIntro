a90e002442918ae52a14c4d072a5db8c75af5016
Svetlin Svilenov <s.e.svilenov@gmail.com>
2020-06-15 02:19:17 +0300

    Project version changed. Basic triggered cutscene created

---
+++ CineIntro/Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class CinematicTrigger : MonoBehaviour
+{
+    public event Action cutSceneTriggeredEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            cutSceneTriggeredEvent?.Invoke();
+        }
+    }
+}
---
+++ CineIntro/Assets/Scripts/Cinematics/CutSceneDirector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutSceneDirector : MonoBehaviour
+{
+    [SerializeField] private CinematicTrigger _cinematicTrigger;
+
+    private PlayableDirector _playableDirector;
+
+    private bool _isCutSceneTriggered;
+
+    private void Awake()
+    {
+        _playableDirector = GetComponent<PlayableDirector>();
+
+        _cinematicTrigger.cutSceneTriggeredEvent += OnCutSceneTriggered;
+    }
+
+    public void OnCutSceneTriggered()
+    {
+        if (!_isCutSceneTriggered)
+        {
+            _isCutSceneTriggered = true;
+            _playableDirector.Play();
+        }
+    }
+}
